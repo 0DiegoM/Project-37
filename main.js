85b93261537a874735bb443ef2db3e77bd08e7ce
0DiegoM <98572984+0DiegoM@users.noreply.github.com>
2023-01-12 19:47:30 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function gotResults(error, results) {
 
 function draw() {
     image(video, 0, 0, 380, 380);
-if(status != undefined) {
+if(status != "") {
 objectDetector.detect(video, gotResults);
 
 for(i = 0; i < objects.length; i++) {
@@ -39,21 +39,22 @@ text(objects[i].label + " "+ percent+ "%", objects[i].x + 15, objects[i].y + 15)
 noFill();
 stroke('red');
 rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-
+if(objects[i].label == objName) {
+    video.stop();
+    objectDetector.detect(gotResults);
+    document.getElementById("objs").innerHTML = "Se encontro "+ objName;
+    synth = window.speechSynthesis;
+    utterThis = SpeechSynthesisUtterance(objName + " encontrado.");
+    synth.speak(utterThis);
+    } else {
+    document.getElementById("objs").innerHTML = objName + " no fue encontrado";
+    }
 }
 }
 }
 
 function start() {
-    if(objects[i].label == document.getElementById("objName").label) {
-        video.stop();
-        objectDetector.detect(gotResults);
-        document.getElementById("status").innerHTML = "Se encontro "+objects[i].label;
-        synth = window.speechSynthesis;
-        utterThis = SpeechSynthesisUtterance(objects[i].label+" encontrado.");
-        synth.speak(utterThis);
-        } else {
-        document.getElementById("status").innerHTML = document.getElementById("objName").label + " no fue encontrado";
-        }
-
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status - Detecting Objects";
+    objName = document.getElementById("objName").value;
 }
